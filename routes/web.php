03df7205132494aa74eba6e566ad8b9a161ff357
tuan <78714721+Chaiyanan8308@users.noreply.github.com>
2025-09-28 23:43:18 +0700

    dupe code from workshop
    dupe code from workshop

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CartController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/product', [App\Http\Controllers\ProductController::class, 'index']);
+Route::post('/product/search', [App\Http\Controllers\ProductController::class, 'search']);
+Route::get('/product/edit/{id?}', [App\Http\Controllers\ProductController::class, 'edit']);
+Route::post('/product/update', [App\Http\Controllers\ProductController::class, 'update']);
+Route::post('/product/add', [App\Http\Controllers\ProductController::class, 'insert']);
+Route::get('/product/remove/{id}', [App\Http\Controllers\ProductController::class, 'remove']);
+
+Route::get('/category', [App\Http\Controllers\CategoryController::class, 'index']);
+Route::post('/category/search', [App\Http\Controllers\CategoryController::class, 'search']);
+Route::get('/category/edit/{id?}', [App\Http\Controllers\CategoryController::class, 'edit']);
+Route::post('/category/update', [App\Http\Controllers\CategoryController::class, 'update']);
+Route::post('/category/add', [App\Http\Controllers\CategoryController::class, 'insert']);
+Route::get('/category/remove/{id}', [App\Http\Controllers\CategoryController::class, 'remove']);
+
+Route::get('/cart/view', [CartController::class, 'viewCart']);
+Route::get('/cart/add/{id}', [CartController::class, 'addToCart']);
+Route::get('/cart/delete/{id}', [CartController::class, 'deleteCart']);
+Route::get('/cart/update/{id}/{qty}', [CartController::class, 'updateCart']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/logout', [App\Http\Controllers\HomeController::class, 'logout']);
